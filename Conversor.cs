c0ee49977aa4fce8b9566d50beec3be0f6290f41
Guilherme <parra.guioliveira@hotmail.com>
2020-07-19 19:08:28 -0300

    Finalizado

--- Conversor.cs
+++ Conversor.cs
@@ -3,16 +3,25 @@ namespace Aula22
     public static class Conversor
     {
         public static float CotacaoDolar = 5.23f;
-        public static float CotacaoDolarCanadense = 3.89f;
         
-
+        
+        /// <summary>
+        /// Converte o valor de real para dolar
+        /// </summary>
+        /// <param name="valorRS">Determina o valor em reais</param>
+        /// <returns>O valor convertido para dólar</returns>
         public static float RealParaDolar(float valorRS){
             return valorRS/CotacaoDolar;
         }
+        /// <summary>
+        /// Converte o valor de dolar para real
+        /// </summary>
+        /// <param name="valorUSS">Determina o valor em dolar</param>
+        /// <returns>O valor convertido em reais</returns>
         public static float DolarParaReal(float valorUSS){
             return  CotacaoDolar*valorUSS;
         }
-        /*public static float DolarCanadenseParaReal(float valorCS){
-            return CotacaoDolarCanadense*valorCS;*/
-        }
+        
+        
     }
+}
--- Program.cs
+++ Program.cs
@@ -8,8 +8,10 @@ namespace Aula22
         {
             Console.ForegroundColor = ConsoleColor.Blue;
             float valorUSS;
+            float valorRS;
             
-            Console.WriteLine (Conversor.DolarParaReal( valorUSS = float.Parse(Console.ReadLine())));
+            Console.WriteLine (Conversor.DolarParaReal(valorUSS = float.Parse(Console.ReadLine())));
+            Console.WriteLine (Conversor.RealParaDolar(valorRS = float.Parse(Console.ReadLine())));
 
             Console.ResetColor();
         }
